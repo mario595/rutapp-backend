d6f1fe8828d48235393e26519806ab47ce5bbd5a
mario595 <mariomartinez595@gmail.com>
2016-02-02 18:32:37 +0000

    DB configuration

--- settings/base.py
+++ settings/base.py
@@ -65,16 +65,6 @@ ROOT_URLCONF = 'rutapp_backend.urls'
 WSGI_APPLICATION = 'rutapp_backend.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
--- settings/staging.py
+++ settings/staging.py
@@ -1,1 +1,5 @@
+import dj_database_url
 from .base import *
+
+
+DATABASES['default'] =  dj_database_url.config()
