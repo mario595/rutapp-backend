e5a042898262aefcda1e2e904ac76babb77c2d4b
mario595 <mariomartinez595@gmail.com>
2016-02-02 18:37:12 +0000

    first deployment.

--- settings/staging.py
+++ settings/staging.py
@@ -1,5 +1,10 @@
 import dj_database_url
+
 from .base import *
 
 
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {'default': dj_database_url.config()}
+
+DEBUG = True
+
+TEMPLATE_DEBUG = True
