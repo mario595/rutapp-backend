c139070b3a0e22c3ad8c7026d729ccaaf3632bbf
mario595 <mariomartinez595@gmail.com>
2016-02-29 08:51:55 +0000

    Added param to url for delay (only in debug mode)

--- rutapp_restapi/views.py
+++ rutapp_restapi/views.py
@@ -1,6 +1,11 @@
+from time import sleep
+
+from django.conf import settings
 from rest_framework import  viewsets
+
 from rutapp_restapi import serializers
 from rutapp_restapi.models import Walk
+
 
 class WalkViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Walk.objects.all()
@@ -9,3 +14,13 @@ class WalkViewSet(viewsets.ReadOnlyModelViewSet):
             return serializers.WalkSummarySerializer
         if self.action == 'retrieve':
             return serializers.WalkDetailSerializer
+    def initialize_request(self, request, *args, **kwargs):
+        if (settings.DEBUG):
+            if ('delay' in request.GET):
+                try:
+                    delay = int(request.GET['delay'])
+                    if (delay <= 30):
+                        sleep(delay)
+                except:
+                    pass
+        return super(WalkViewSet, self).initialize_request(request, *args, **kwargs)
